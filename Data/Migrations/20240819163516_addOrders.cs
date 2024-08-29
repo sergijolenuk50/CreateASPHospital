30d73b09aa399c250f466f7144c0bf882a99175c
sergijolenuk50 <sergijolenuk50@gmail.com>
2024-08-29 23:20:39 +0300

    createASP

---
+++ Data/Migrations/20240819163516_addOrders.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addOrders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OrderId",
+                table: "Doctors",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Birthdate",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Doctors",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Doctors",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "OrderId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Doctors",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "OrderId",
+                value: null);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Doctors_OrderId",
+                table: "Doctors",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_UserId",
+                table: "Orders",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Doctors_Orders_OrderId",
+                table: "Doctors",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Doctors_Orders_OrderId",
+                table: "Doctors");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Doctors_OrderId",
+                table: "Doctors");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "Doctors");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Birthdate",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+    }
+}
---
+++ WebApplication2/Controllers/OrdersController.cs
@@ -0,0 +1,38 @@
+﻿using Core.Interfaces;
+using Core.Services;
+using Data.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace WebApplication2.Controllers
+{
+    //public class OrdersController : Controller
+    //{
+    //}
+        [Authorize]
+        public class OrdersController : Controller
+        {
+            private readonly IOrdersServices ordersService;
+        private readonly RoleManager<IdentityRole> userManager;
+            private string UserId => User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+
+            public OrdersController(IOrdersServices ordersService)
+            {
+                this.ordersService = ordersService;
+           // this.userManager = userManager;
+            }
+            public IActionResult Index()
+            {
+           // userManager.CreateAsync(new IdentityRole());
+                return View(ordersService.GetOrders(UserId));
+            }
+
+            public IActionResult Create()
+            {
+                ordersService.Create(UserId);
+                return RedirectToAction(nameof(Index));
+            }
+        }
+}
